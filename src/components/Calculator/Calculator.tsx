04b0d2c1166b3b536a91f8be44cb6ca6c0247bcd
Rodrigo Vitiello <rodrigo@vitiello.com.br>
2020-08-20 19:21:18 -0300

    creates calculator component

---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+interface ICalculatorProps { }
+interface ICalculatorState { }
+
+class Calculator extends Component<ICalculatorProps, ICalculatorState> {
+  constructor(props: ICalculatorProps) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return null
+  }
+}
+
+export default Calculator;
