3f952f476ffbec028dc61323ce04835b2621e851
Rodrigo Vitiello <rodrigo@vitiello.com.br>
2020-09-06 10:42:14 -0300

    refactors app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react'
 import './App.scss';
 import Logo from '../Logo/Logo';
 import Calculator from '../Calculator/Calculator';
 
 interface IAppProps {}
-interface IAppState {}
-class App extends Component<IAppProps, IAppState> {
-  constructor(props: IAppProps) {
-    super(props)
-  }
+const App = (props: IAppProps) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <h1>Calculadora de Coxinhas</h1>
+      </header>
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Logo />
-          <h1>Calculadora de Coxinhas</h1>
-        </header>
-
-        <Calculator />
-      </div>
-    );
-  }
+      <Calculator />
+    </div>
+  )
 }
 
 export default App;
